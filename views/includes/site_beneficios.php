0741002d72fbc5f373f708a83cab291e486890e2
Aristeu Garcia Goncalves <aristeu_garcia@outlook.com>
2016-05-06 18:09:45 -0300

    Merge alterações foto, beneficios

---
+++ views/includes/site_beneficios.php
@@ -0,0 +1,63 @@
+<style>
+.beneficios p{
+    font-size: 20px;
+}
+.beneficios {
+    background-image: url(assets/images/background/bg_beneficios.jpg);
+    padding-top: 46px;
+    padding-bottom: 14px;
+    color: #fff;
+}
+.bg{
+        background: rgba(71,157,80,1);
+        background: -moz-linear-gradient(-45deg, rgba(71,157,80,1) 0%, rgba(65,153,129,1) 100%);
+        background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(71,157,80,1)), color-stop(100%, rgba(65,153,129,1)));
+        background: -webkit-linear-gradient(-45deg, rgba(71,157,80,1) 0%, rgba(65,153,129,1) 100%);
+        background: -o-linear-gradient(-45deg, rgba(71,157,80,1) 0%, rgba(65,153,129,1) 100%);
+        background: -ms-linear-gradient(-45deg, rgba(71,157,80,1) 0%, rgba(65,153,129,1) 100%);
+        background: linear-gradient(135deg, rgba(71,157,80,1) 0%, rgba(65,153,129,1) 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#479d50', endColorstr='#419981', GradientType=1 );
+        
+    }
+.beneficios .title-h1 {
+    font-size: 4.3em;
+    font-weight: 900;
+    color: #fff;
+}
+.beneficios .row {
+    padding: 20px 0;
+}
+.spacerow{
+    line-height: 35%
+}
+.text{
+
+    position: absolute;
+    margin-top: -51px;
+    margin-left: 50px;
+}
+
+</style>
+
+<section id="beneficios" class="section why beneficios bg">
+    <div class="container">
+        <h2 class="title text-center"></h2>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 " >
+                <p class="title-h1 spacerow" >Benefícios<img src="assets/images/background/x-beneficios.png"/></p>
+                <p style="font-size: 2.3em;">para as startups</p>
+                <p>Selecionadas <b>5 startups</b> na primeira rodada, que contarão com 3 meses de:</p><br>
+                <div class="desc" >
+                    <img src="assets/images/figures/beneficios_quadrados_branco.png"><p class="text"><b>Oportunidades</b> </p>
+                        <p style="margin-left: 103px;">para fazer negócios com empresas do grupo Fischer e seus clientes</p>
+                    <img src="assets/images/figures/beneficios_quadrados_branco.png"><p class="text"><b>Metodologia</b> </p>
+                        <p style="margin-left: 103px;">todos os serviços de um grupo full service  de comunicação e marketing, uma das maiores e mais premiadas agências do BR (planejamento estratégico, branding, criação e desenvolvimento de comunicação, estratégia de crescimento)</p>
+                    <img src="assets/images/figures/beneficios_quadrados_branco.png"><p class="text"><b>Estrutura</b> </p>
+                        <p style="margin-left: 103px;"> serviços e ferramentas do <b>Grupo Fischer</b></p>
+                    <img src="assets/images/figures/beneficios_quadrados_branco.png"><p class="text"><b>Plataforma</b> </p>
+                        <p style="margin-left: 103px;">networking para gerar conhecimento e oportunidades de negócio</p>
+                </div>
+            </div><!--//content-->
+        </div>
+    </div><!--//item-->
+</section>
--- views/site_index.php
+++ views/site_index.php
@@ -11,27 +11,8 @@
 <!-- ******Testimonials Section****** -->
 <?php include 'includes/site_testemonial.php'; ?>
 
-<section id="sobre" class="section why">
-    <div class="container">
-        <h2 class="title text-center"></h2>
-        <div class="row item ">
-            <div class="content col-md-4 col-sm-12 col-xs-12">
-                <h3 class="title-h2">Benefícios para as startups</h3><br>
-                <div class="desc">
-                    <p>Selecionadas 5 startups na primeira rodada, que contarão com 3 meses de:</p>
-                    <p>&bull; Oportunidades para fazer negócios com empresas do grupo Fischer e seus clientes</p>
-                    <p>&bull;  Receber todos os serviços de um grupo full service  de comunicação e marketing, uma das maiores e mais premiadas agências do BR (planejamento estratégico, branding, criação e desenvolvimento de comunicação, estratégia de crescimento)
-                    </p>
-                    <p>&bull; Utilizar serviços e ferramentas do Grupo Fischer</p>
-                    <p>&bull; Plataforma de networking para gerar conhecimento e oportunidades de negócio</p>
-                </div>
-            </div><!--//content-->
-            <figure class="figure col-md-7 col-sm-12 col-xs-12 col-md-offset-1 col-sm-offset-0 col-xs-offset-0">
-                <img class="img-responsive" src="assets/images/figures/figure-3.png" alt="" />
-                <figcaption class="figure-caption">(Screenshot: <a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-startups-tempo/?ref=3wm" target="_blank">Tempo - Bootstrap template for startups)</a> </figcaption>
-                </figure>
-        </div><!--//item-->
-    </section>
+<!-- ******Beneficios para startups****** -->
+<?php include 'includes/site_beneficios.php'; ?>
 
 
 
@@ -48,12 +29,12 @@
                         <br><p>&bull; <b>Startups</b> que permitam conexão e comunicação com os públicos e negócios dos clientes pertencentes ao Grupo Fischer</p>
                         <br><p>&bull; <b>Projetos</b> que tenham a ver com o contexto de negócios das seguintes áreas (produtos ou serviços) e focados em B2C</p>
                         <br><p style="text-align: center;">&bull; <br>Melhoria na relação com o dinheiro<label style="color: #2A8C37"> (vida financeira)</label></p>
-                        <p style="text-align: center;">&bull; <br>Produtos ou serviços que visam a melhoria da relação com o corpo e a saúde</p>
-                        <p style="text-align: center;">&bull; <br>Processo de escolha e/ou compra de produtos e serviços</p>
-                        <p style="text-align: center;">&bull; <br>Processo de escolha e/ou compra de produtos e serviços ligados a entretenimento <label style="color: #2A8C37">(viagem, cinema, restaurante, férias, hotel etc.)</label></p>
-                        <p style="text-align: center;">&bull; <br>Produtos ou serviços de inteligência para o varejo</p>
-                        <br><p style="text-align: center;">&bull; <br>Serviços ligados à entretenimento esportivo<label style="color: #2A8C37"> (Futebol)</label></p>
-                        <p style="text-align: center;">&bull; <br>Inteligências em segmentação e compra de mídia</p>
+                        <p >&bull; <br>Produtos ou serviços que visam a melhoria da relação com o corpo e a saúde</p>
+                        <p >&bull; <br>Processo de escolha e/ou compra de produtos e serviços</p>
+                        <p >&bull; <br>Processo de escolha e/ou compra de produtos e serviços ligados a entretenimento <label style="color: #2A8C37">(viagem, cinema, restaurante, férias, hotel etc.)</label></p>
+                        <p >&bull; <br>Produtos ou serviços de inteligência para o varejo</p>
+                        <br><p>&bull; <br>Serviços ligados à entretenimento esportivo<label style="color: #2A8C37"> (Futebol)</label></p>
+                        <p >&bull; <br>Inteligências em segmentação e compra de mídia</p>
                     </div>
                 </div><!--//content-->
             </div><!--//item-->
