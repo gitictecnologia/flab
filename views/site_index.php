f0af870381cd96bd8f2f03863fac948e89c285cc
Aristeu Garcia Goncalves <aristeu_garcia@outlook.com>
2016-05-06 15:55:37 -0300

    Merge quebra de testemonial

--- views/site_index.php
+++ views/site_index.php
@@ -9,27 +9,7 @@
 <div class="sections-wrapper">
 
 <!-- ******Testimonials Section****** -->
-<section class="section testimonials">
-    <figure class="profile" style="text-align: center;"><img class="img-circle"src="assets/images/people/jairocircle.png" width="300" height="300" alt="" /></figure><br><br>
-    <div class="container">
-        <div id="testimonials-carousel" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="item active">
-                    <div class="content">
-                        <blockquote>
-                            <i class="fa fa-quote-left" style="font-size: 41px"></i>
-                            <p>Acreditamos que a inovação e a evolução do negócio de comunicação e marketing passam também pelo incentivo, 
-                                pela aproximação e pela participação de iniciativas empreendedoras, 
-                                pioneiras e desbravadoras, que permitem estarmos conectados e antenados, 
-                                sendo geradoras de aprendizados e conhecimento mútuo entre todas as partes do nosso negócio.
-                            </p>
-                        </blockquote>
-                    <p class="source">Jairo Soares &nbsp;<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i><br /><span class="title">Idealizador do FLAB.SOLUTIONS e COO da agência Fischer America</span></p>
-                </div><!--//content-->                         
-            </div><!--//item-->                    
-        </div><!--//carousel-->
-    </div><!--//container-->
-</section><!--//testimonials-->
+<?php include 'includes/site_testemonial.php'; ?>
 
 <section id="sobre" class="section why">
     <div class="container">
