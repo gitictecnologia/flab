98ad9ff843fa509849b0c5aa986207b18a45bb2d
Aristeu Garcia Goncalves <aristeu_garcia@outlook.com>
2016-05-06 16:26:37 -0300

    Acerto de testemonial + css testemonial

--- views/includes/site_testemonial.php
+++ views/includes/site_testemonial.php
@@ -6,14 +6,15 @@
                 <div class="item active">
                     <div class="content">
                         <blockquote>
-                            <i class="fa fa-quote-left" style="font-size: 41px"></i>
+                            <i class="fa fa-quote-left" style="font-size: 41px; left: -45px;"></i>
                             <p>Acreditamos que a inovação e a evolução do negócio de comunicação e marketing passam também pelo incentivo, 
                                 pela aproximação e pela participação de iniciativas empreendedoras, 
                                 pioneiras e desbravadoras, que permitem estarmos conectados e antenados, 
-                                sendo geradoras de aprendizados e conhecimento mútuo entre todas as partes do nosso negócio.
+                                sendo geradoras de aprendizados e conhecimento mútuo entre todas as partes do nosso negócio.<i class="fa fa-quote-right" style="''"></i>
                             </p>
                         </blockquote>
-                        <p class="source">Jairo Soares &nbsp;<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i><br /><span class="title">Idealizador do FLAB.SOLUTIONS e COO da agência Fischer America</span></p>
+                        <a href="#"><p class="source">Jairo Soares &nbsp;<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i><br /></a>
+                            <span class="title">Idealizador do FLAB.SOLUTIONS e COO da agência Fischer America</span></p>
                     </div><!--//content-->                         
                 </div><!--//item-->                    
             </div><!--//carousel-->
