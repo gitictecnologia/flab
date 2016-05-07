1c6db6d088f0049633a29c3fb79cea8e15311428
ruimariogomes <ruimariogomes@gmail.com>
2016-05-07 02:01:10 -0300

    Interesses layout 99.9% feito

--- views/includes/site_interesses.php
+++ views/includes/site_interesses.php
@@ -1,11 +1,46 @@
 
 <style>
+    .interesses {
+        background-color: #E1E1E1;
+    }
     .interesses .title-h1 {
         font-size: 4.7em;
         font-weight: 900;
         color: #535353;
         margin-bottom: 5px;
         margin-top: 50px;
+    }
+    .x-box {
+        border: 6px solid #73FA79;
+        margin: 5px;
+        padding: 20px;
+        min-height: 340px;
+    }
+    .x-box h1, .x-box h2 {
+        font-size: 3em;
+        font-weight: 300;
+        margin: 0px;
+    }
+    .x-box h1 {        
+        font-weight: 900;
+    }
+    .x-box p {
+        margin-top: 15px;
+        font-size: 1.2em;
+    }
+    .interesses-exp {
+        font-size: 1.2em;
+        font-weight: 300;
+    }
+    .interesses-exp span {
+        margin-left: 30px;
+        margin-right: 40px;
+    }
+
+    @media (min-width: 992px) {
+        .col-md-10.x-box {
+            width: 84.33333333%;
+        }
     }
 </style>
 <section id="interesses" class="interesses">           
@@ -14,29 +49,38 @@
         <div class="col-md-8">
             <h1 class="title-h1">Área de Interesse</h1>
             
-                <div class="row">
-                    <div class="col-md-5">
-                        <h1>Requisito</h1>
-                        <h2>Principal</h2>
-                        <p>presença nas plataformas mobile</p>
-                    </div>
-                    <div class="col-md-5"></div>
+            <div class="row">
+                <div class="col-md-5 x-box">
+                    <h1>Requisito</h1>
+                    <h2>Principal</h2>
+                    <p>presença nas plataformas mobile</p>
                 </div>
-                <div class="row">
-                    <div class="col-md-10"></div>                    
-                </div>
-
-                <p>&bull; <b></b> presença nas plataformas mobile</p>
-                <br><p>&bull; <b>Startups</b> que permitam conexão e comunicação com os públicos e negócios dos clientes pertencentes ao Grupo Fischer</p>
-                <br><p>&bull; <b>Projetos</b> que tenham a ver com o contexto de negócios das seguintes áreas (produtos ou serviços) e focados em B2C</p>
-                <br><p style="text-align: center;">&bull; <br>Melhoria na relação com o dinheiro<label style="color: #2A8C37"> (vida financeira)</label></p>
-                <p >&bull; <br>Produtos ou serviços que visam a melhoria da relação com o corpo e a saúde</p>
-                <p >&bull; <br>Processo de escolha e/ou compra de produtos e serviços</p>
-                <p >&bull; <br>Processo de escolha e/ou compra de produtos e serviços ligados a entretenimento <label style="color: #2A8C37">(viagem, cinema, restaurante, férias, hotel etc.)</label></p>
-                <p >&bull; <br>Produtos ou serviços de inteligência para o varejo</p>
-                <br><p>&bull; <br>Serviços ligados à entretenimento esportivo<label style="color: #2A8C37"> (Futebol)</label></p>
-                <p >&bull; <br>Inteligências em segmentação e compra de mídia</p>
+                <div class="col-md-5 x-box" style="padding-top: 70px;">
+                    <h1>Startups</h1>                        
+                    <p>que permitam conexão e comunicação com os públicos e negócios dos clientes pertencentes ao <strong>Grupo Fischer</strong></p>
+                </div>                    
             </div>
+            <div class="row">
+                <div class="col-md-10 x-box">
+                    <h1>Projetos</h1>
+                    <p>que tenham a ver com o contexto de negócios<br> das seguintes áreas (produtos ou serviços)<br> e focados em B2C</p> 
+                </div>                    
+            </div>               
+                          
+            <br>
+            <div class="interesses-exp">
+                <p><span>&bull;</span> Melhoria na relação com o dinheiro (vida financeira)</p>
+                <p><span>&bull;</span> Produtos ou serviços que visam a melhoria da relação com o corpo e a saúde</p>
+                <p><span>&bull;</span> Processo de escolha e/ou compra de produtos e serviços</p>
+                <p style="margin-bottom: 0px;"><span>&bull;</span> Processo de escolha e/ou compra de produtos e serviços ligados a entretenimento
+                <p style="margin-top: 0px;"><span>&nbsp;</span> (viagem, cinema, restaurante, férias, hotel etc.)</p>
+                <p><span>&bull;</span> Produtos ou serviços de inteligência para o varejo</p>
+                <p><span>&bull;</span> Serviços ligados à entretenimento esportivo (Futebol)</p>
+                <p><span>&bull;</span> Inteligências em segmentação e compra de mídia</p>
+                <br>
+                <br>
+                <br>
+            </div>            
         </div>
         <div class="col-md-2"></div>
     </div>   
