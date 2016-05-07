30e3e63411422505c631bff37271324a29db7b64
mrgcoder <rmgcoder@gmail.com>
2016-05-07 11:35:38 -0300

    Ajuste de texto

--- views/includes/site_lightbox.php
+++ views/includes/site_lightbox.php
@@ -38,7 +38,12 @@
 
 				<div class="row">
 					<div class="col-sm-12">
-						<p class="lightbox-texto">Com 22 anos de experiência em mídia e negócios em comunicação com passagens nas principais agências e um dos pioneitos no Brasil em Digital, com passagens pelo Vale do Silício, atualmente está a frente da Operação da Agência de publicidade <strong>Fischer</strong>, uma das maiores do mercado, atuando também como líder do departamento de mídia e responsável pela restruturação do <strong>Grupo Fischer</strong> na área Digital e de Inovação. Desde 2014 atua como conselheiro e investidor anjo em statups do Brasil e Estados Unidos</p>
+						<p class="lightbox-texto">
+							Com 22 anos de experiência em mídia e negócios em comunicação com passagens nas principais agências e um dos pioneiros no Brasil em Digital,
+							com passagens pelo Vale do Silício, atualmente está a frente da Operação da Agência de publicidade <strong>Fischer</strong>, uma das maiores do mercado,
+							atuando também como líder do departamento de mídia e responsável pela restruturação do <strong>Grupo Fischer</strong> na área Digital e de Inovação.
+							Desde 2014 atua como conselheiro e investidor anjo em statups do Brasil e Estados Unidos
+							</p>
 					</div>
 				</div>
 				<div class="row">
