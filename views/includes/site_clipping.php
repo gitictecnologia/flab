523f3f71724ac248183e072ecbea06d4e8d0b49f
Aristeu Garcia Goncalves <aristeu_garcia@outlook.com>
2016-05-30 09:49:41 -0300

    Aristeu- alteração de clipping para news

--- views/includes/site_clipping.php
+++ views/includes/site_clipping.php
@@ -87,7 +87,7 @@
     <div class="row">
         <div class="col-md-2"></div>
         <div class="col-md-8">
-            <p class="title-h1" >Clipping</p>
+            <p class="title-h1" >News</p>
             
 
             <?php
